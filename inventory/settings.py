c270f38da31e7c1020088abb784a6236ae3b8cfd
arissaap <arissa@student.uns.ac.id>
2021-02-26 10:42:38 +0700

    Update settings.py

--- inventory/settings.py
+++ inventory/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'osmqv39b1!f(byza&z#$$49cgbt23szh!rxf#l2&nvc#ae@l37'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
